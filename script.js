f7b7e4c26434cc69c5dda6975536b19878e338b2
jason <jason_tan75@hotmail.com>
2024-09-14 21:31:38 -0400

    styling

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-const pages = []; // list of objects with URL(required) and img&name(optional) keys
+let pages = localStorage.getItem('pages') ? localStorage.getItem('pages') : []; // list of objects with URL(required) and img&name(optional) keys
 // store URLs as list with separate object for customized display?
-const textlist = [];
-for (let i = 0; i < 100; i++) {
-    textlist.push(i);
-}
 
+
+// nav
+const favouritesLink = document.getElementById('link-favourites');
+const addLink = document.getElementById('link-add');
+const pagesLink = document.getElementById('link-pages');
+
+// favourites
+const favouritesEl = document.getElementById('favourites');
 
 // add page form
+const addForm = document.getElementById('page-add');
 const nameInput = document.getElementById('input-name');
 const imgInput = document.getElementById('input-img');
 const urlTextarea = document.getElementById('url');
@@ -18,46 +23,95 @@ const pageCollection = document.getElementById('page-collection');
 const pageListContainer = document.getElementById('page-lists-container');
 const pageList = document.getElementById('page-list');
 
+// lists
+const formInputs = [
+    nameInput,
+    imgInput,
+    urlTextarea,
+];
+const displayElements = [
+    favouritesEl,
+    addForm,
+    pageCollection,
+];
+const navLinks = [
+    favouritesLink,
+    addLink,
+    pagesLink,
+];
+
 
 function storeToLocal() {
-    localStorage.setItem('tabs', JSON.stringify(tabs));
+    // functions don't work, maybe hoisting / timing issue
+    // try using parameters
+    localStorage.setItem('pages', JSON.stringify(pages));
 };
 // add optional parameters for tab organization
 function getFromLocal() {
-    localStorage.getItem('tabs');
+    localStorage.getItem('pages');
 };
 
-btn1.addEventListener('click', () => {
+btn1.addEventListener('click', (e) => {
+    e.preventDefault();
     // add check for duplicate url, name and image can be reused, though discouraged
+
     pages.push({
         name: nameInput.value,
         image: imgInput.value,
         url: urlTextarea.value,
     });
+    for (const input of formInputs) {
+        input.value = '';
+    };
+    localStorage.setItem('pages', JSON.stringify(pages));
+    renderList();
 });
 
 btn2.addEventListener('click', (e) => {
     e.preventDefault();
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        console.log(tabs[0].url); // working in extension instance
+        // console.log(tabs[0].url); // working in extension instance
         nameInput.value = tabs[0].title;
         imgInput.value = tabs[0].favIconUrl;
         urlTextarea.value = tabs[0].url; // enter data but don't submit
         // add separate button for autofill from active tab
-        console.log(tabs); // check tabs data scheme
+        // console.log(tabs); // check tabs data scheme
     })
 });
 
 
-function renderList(arr) {
+function toggleDisplays(el) {
+    for (const element of displayElements) {
+        // console.log(el.classList[0] === link.classList[0]);
+        if (el.classList[0] === element.classList[0]) {
+            element.style.display = 'block';
+        } else {
+            element.style.display = 'none';
+        };
+    };
+};
+
+function renderList() {
     let stringList = '';
-    for (const item of textlist) {
-        stringList += `<li>${item}</li>`;
+    pages = JSON.parse(localStorage.getItem('pages'));
+    console.log(pages);
+    for (const page of pages) {
+        stringList += `
+        <a href='${page.url}' target='_blank' class='page-link' name=${page.name} >
+            <li class='page-li' >
+                <img class='page-image' src=${page.image} alt='${page.name} favicon' />
+            </li>
+        </a>`;
     }
     pageList.innerHTML = stringList;
 };
 
-renderList();
+
+for (const link of navLinks) {
+    link.addEventListener('click', (e) => {
+        toggleDisplays(e.target);
+    });
+};
 
 // Stretch
 // add account func to use cloud server for back-up
